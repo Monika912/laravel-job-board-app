483a76f515f3bef0c6bddd57f63cd58d2c012f8a
Monika <amonika8754@gmail.com>
2026-03-12 22:23:26 +0530

    Added css for Show

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -23,8 +23,9 @@
             </p>
 
             <div>
-                <a href="{{ route('jobs.show', $job) }}">
-                    See
+                <a href="{{ route('jobs.show', $job) }}"
+                class="rounded-md border border-slate-300 bg-white px-2.5 py-1.5 text-center text-sm font-semibold text-black shadow-sm hover:bg-slate-100">
+                    Show
                 </a>
             </div>
         </x-card>
