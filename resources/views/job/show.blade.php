11c5c274e7b7001336d14707fdfe932838933c58
Monika <amonika8754@gmail.com>
2026-03-29 20:20:58 +0530

    Resolve dthe error of array

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <x-breadcrumbs :$job class="mb-4"
+    <x-breadcrumbs class="mb-4"
     :links="['Jobs' => route('jobs.index'), $job->title => '#']"/>
     <x-job-card :$job />
 </x-layout>
