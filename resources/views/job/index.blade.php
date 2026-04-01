ed4cb811753004076e5c2605945d1b38bb977836
Monika <amonika8754@gmail.com>
2026-04-01 11:26:49 +0530

    Modified files

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -7,16 +7,25 @@
 
     <x-breadcrumbs :links="$links" class="mb-4" />
     <x-card class="mb-4 text-sm">
-        <div class="mb-4 grid grid-cols-2 gap-4">
-            <div>
-                <div class="mb-1 font-semibold">Search</div>
-                <x-text-input name="search" value="" placeholder="Search for any text"/>
-            </div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
+        <form action="{{ route('jobs.index') }}" method="GET">
+            <div class="mb-4 grid grid-cols-2 gap-4">
+                <div>
+                    <div class="mb-1 font-semibold">Search</div>
+                    <x-text-input name="search" value="" placeholder="Search for any text" />
+                </div>
+                <div>
+                    <div class="mb-1 font-semibold">Salary</div>
+                    <div class="flex space-x-2">
+                        <x-text-input name="min_salary" value="" placeholder="From" />
+                        <x-text-input name="max_salary" value="" placeholder="To" />
+                    </div>
+                </div>
+                <div>3</div>
+                <div>4</div>
 
-        </div>
+            </div>
+            <button class="w-full">Filter</button>
+        </form>
     </x-card>
     @foreach ($jobs as $job)
         <x-job-card class="mb-4" :$job>
@@ -28,7 +37,6 @@
 
             </div>
 
-        </x-card>
-
+            </x-card>
     @endforeach
-</x-layoutx-layout>
+    </x-layoutx-layout>
--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -8,5 +8,9 @@
 
     <x-breadcrumbs :links="$links" class="mb-4" />
 
-    <x-job-card :$job />
+    <x-job-card :$job>
+        <p class="text-sm text-slate-500 mb-4">
+                {!! nl2br(e($job->description)) !!}
+            </p>
+    </x-job-card>
 </x-layout>
