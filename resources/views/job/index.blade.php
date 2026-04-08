8dfa22e3b7531dd582297160584932f744187d58
Monika <amonika8754@gmail.com>
2026-04-08 11:41:58 +0530

    Adding Styles and Alphine.js

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -53,7 +53,7 @@
                 </div>
 
             </div>
-            <button class="w-full">Filter</button>
+            <x-button class="w-full">Filter</x-button>
         </form>
     </x-card>
     @foreach ($jobs as $job)
