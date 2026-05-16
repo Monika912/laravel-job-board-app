7ad10822cfdcbfe237c9dcaab8938705f1dbd594
Monika <amonika8754@gmail.com>
2026-05-16 19:17:47 +0530

    Employer:Searching By Employer Name

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -17,23 +17,28 @@ class Job extends Model
     public static array $experience = ['entry', 'intermediate', 'senior'];
     public static array $category = ['IT', 'Finance', 'Marketing', 'Sales', 'HR'];
 
-    public function employer(){
+    public function employer()
+    {
         return $this->belongsTo(Employer::class);
     }
 
-    public function scopeFilter(Builder | QueryBuilder $query, array $filters){
-        return $query->when($filters['search'] ?? null, function($query, $search) use ($filters){
-            $query->where(function ($query) use ($filters) {
+    public function scopeFilter(Builder | QueryBuilder $query, array $filters)
+    {
+        return $query->when($filters['search'] ?? null, function ($query, $search) use ($filters) {
+            $query->where(function ($query) use ($filters, $search) {
                 $query->where('title', 'like', '%' . $filters['search'] . '%')
-            ->orWhere('description', 'like', '%' . $filters['search'] . '%');
+                    ->orWhere('description', 'like', '%' . $filters['search'] . '%')
+                    ->orWhereHas('employer', function ($query) use ($search) {
+                        $query->where('company_name', 'like', '%' . $search . '%');
+                    });
             });
-        })->when($filters['min_salary'] ?? null, function($query) use ($filters){
+        })->when($filters['min_salary'] ?? null, function ($query) use ($filters) {
             $query->where('salary', '>=', $filters['min_salary']);
-        })->when($filters['max_salary'] ?? null, function($query) use ($filters){
+        })->when($filters['max_salary'] ?? null, function ($query) use ($filters) {
             $query->where('salary', '<=', $filters['max_salary']);
-        })->when($filters['experience'] ?? null, function($query) use ($filters){
+        })->when($filters['experience'] ?? null, function ($query) use ($filters) {
             $query->where('experience', $filters['experience']);
-        })->when($filters['category'] ?? null, function($query) use ($filters) {
+        })->when($filters['category'] ?? null, function ($query) use ($filters) {
             $query->where('category', $filters['category']);
         });
     }
