8b678a1640076516bb98a05f65b602aa30fd46e7
Monika <amonika8754@gmail.com>
2026-06-12 19:18:57 +0530

    Autentication: Added styles for sign in form

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -5,7 +5,12 @@
             @csrf
 
             <div class="mb-8">
-                <label for="">Email</label>
+                <label for="email" class="mb-2 block text-sm font-medium text-slate-900">Email</label>
+                <x-text-input name="email" />
+            </div>
+            <div class="mb-8">
+                <label for="password" class="mb-2 block text-sm font-medium text-slate-900">Password</label>
+                <x-text-input name="password" type="password"/>
             </div>
 
             <div class="mb-8 flex justify-between text-sm font-medium">
