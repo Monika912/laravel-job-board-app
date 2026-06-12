60910205f85b2400118b90555ef9aeb5ae32d151
Monika <amonika8754@gmail.com>
2026-06-12 20:00:34 +0530

    Authentication: Modified layout.balde.php to output the user

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,5 +9,6 @@
     </head>
     <body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-200 from-10% via-sky-200 via-30% to-emerald-200 to-90% text-slate-700">
         {{ $slot }}
+        {{ auth()->user()->name ?? 'Guest' }}
     </body>
 </html>
