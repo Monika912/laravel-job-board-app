e96a00ddf7fa4f03c4a6995d10b9b43bc319f8f1
Monika <amonika8754@gmail.com>
2026-06-12 19:36:58 +0530

    Aunthentication: UI for Sign in Form

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -7,6 +7,20 @@
             <div class="mb-8">
                 <label for="">Email</label>
             </div>
+
+            <div class="mb-8 flex justify-between text-sm font-medium">
+                <div>
+                    <div class="flex items-center space-x-2">
+                        <input type="checkbox" name="remember" class="rounded-sm border border-slate-500">
+                        <label for="remember">Remember me</label>
+                    </div>
+                </div>
+                <div>
+                    <a href="#" class="text-indigo-600 hover:underline">Forget password?</a>
+                </div>
+            </div>
+
+            <x-button class="w-full bg-green-50">Login</x-button>
         </form>
     </x-card>
 </x-layout>
