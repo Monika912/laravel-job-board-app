79ef713fd331b23102e273e7c0790983e0cc479d
Monika <amonika8754@gmail.com>
2026-06-12 18:23:49 +0530

    Aunthentication:Sign in Form

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -1,5 +1,12 @@
 <x-layout>
-    <x-card>
-        Login!
+    <h1 class="my-16 text-center text-4xl font-medium text-slate-600">Sign in to your account</h1>
+    <x-card class="py-8 px-16">
+        <form action="{{ route('auth.store') }}" method="POST">
+            @csrf
+
+            <div class="mb-8">
+                <label for="">Email</label>
+            </div>
+        </form>
     </x-card>
 </x-layout>
