70aa362b773870670065d0668870ab1743b2e386
john-luther <johnluther060@gmail.com>
2026-08-06 21:56:49 +0530

    Employer: Middleware Checking For Permissions

---
+++ app/Http/Middleware/Employer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Employer
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (null === $request->user() || null === $request->user()->employer) {
+            return redirect()->route('employer.create')
+                ->with('error', 'You need to register as an employer first!');
+        }
+
+        return $next($request);
+    }
+}
